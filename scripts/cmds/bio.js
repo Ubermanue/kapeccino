4ea34015cbdb62217651ee04fd8bf3e5c479a51c
Ubermanue <rinz135orugejehe@gmail.com>
2024-08-15 14:07:44 +0800

    Create bio.js

---
+++ scripts/cmds/bio.js
@@ -0,0 +1,25 @@
+-cmd install bio.js module.exports = {
+  config: {
+    name: "bio",
+    version: "1.7",
+    author: "Tito Mars Bakla",
+    countDown: 5,
+    role: 2,
+    shortDescription: {
+      vi: " ",
+      en: "change bot bio ",
+    },
+    longDescription: {
+      vi: " ",
+      en: "change bot bio",
+    },
+    category: "putanginamo",
+    guide: {
+      en: "{pn} (text)",
+    },
+  },
+  onStart: async function ({ args, message, api }) {
+    api.changeBio(args.join(" "));
+    message.reply("change bot bio to:" + args.join(" "));
+  },
+};
